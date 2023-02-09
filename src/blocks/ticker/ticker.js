f6b2a116ab2357d98f6a0780c05227ddb38c9a55
des-yogi <nuevo-design@mail.ru>
2023-02-09 20:33:05 +0200

    ticker scroll fix

--- src/blocks/ticker/ticker.js
+++ src/blocks/ticker/ticker.js
@@ -1,6 +1,7 @@
 (function(){
   const ticker = document.querySelector('.ticker');
   const list = document.querySelector('.ticker__list');
+  if (!list) return;
   const clone = list.cloneNode(true);
   ticker.append(clone);
 }());
